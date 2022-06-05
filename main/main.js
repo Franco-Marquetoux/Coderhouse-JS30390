2ab0bf3c1bf6e7c05ca0e188e4728798101ddd1e
Franco-Marquetoux <franco.marquetoux1@outlook.es>
2022-06-05 20:08:45 -0300

    Uso de CASE, y mejor organizacion

--- main/main.js
+++ main/main.js
@@ -1,55 +1,191 @@
-let alumnos = [];
-let cursos = [];
+const MENU_info = `
+Elija una de las opciones del siguiente menu:
+  1. Crear alumno
+  2. Crear cursos
+  3. Listar alumnos
+  4. Listar cursos
+  5. Eliminar alumno
+  6. Eliminar curso
+  7. Agregar alumno a un curso
+  8. Mostrar detalles de un curso
+  9. Mostrar itinerario
+  0. Salir
+`;
 
-class Alumnos {
-  constructor(nombre, apellido, curso) {
-    (this.nombre = nombre), (this.apellido = apellido), (this.curso = curso);
+const ACCIONES = {
+  CrearEstudiante: 1,
+  CrearCurso: 2,
+  ListadoEstudiantes: 3,
+  ListadoCursos: 4,
+  BorrarEstudiante: 5,
+  BorrarCurso: 6,
+  AgregarEstudianteACurso: 7,
+  MostrarCurso: 8,
+  MostrarAgenda: 9,
+  Salir: 0,
+};
+
+class Estudiante {
+  constructor(nombre, apellido, dni) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.id = dni;
+  }
+
+  nombreCompleto() {
+    return `🙍‍♂️ ${this.nombre} ${this.apellido}`;
   }
 }
 
-class Cursos {
-  constructor(nombre, iniciaFecha) {
-    (this.nombre = nombre),
-      (this.fechaIni = iniciaFecha),
-      (this.cantidadDeAlumnos = 0);
+class Curso {
+  constructor(nombre, fechaInicio, vacantes = 5) {
+    this.nombre = nombre;
+    this.fechaInicio = fechaInicio;
+    this.vacantes = vacantes;
+    this.estudiantes = [];
+  }
+
+  agregarEstudiante(estudiante) {
+    if (this.estudiantes.length >= this.vacantes) {
+      alert("Cupo lleno");
+      return;
+    }
+
+    this.estudiantes.push(estudiante);
+  }
+
+  info() {
+    const estudiantes = this.estudiantes
+      .map((s) => s.nombreCompleto())
+      .join("\n");
+
+    return `
+      Titulo: ${this.nombre},
+      Fecha de inicio: ${this.fechaInicio},
+      Alumnos: ${estudiantes}
+    `;
   }
 }
-cursos.push(new Cursos("BackEnd", "17/07/22"));
-cursos.push(new Cursos("React js", "18/07/22"));
-cursos.push(new Cursos("Vue js", "19/07/22"));
-cursos.push(new Cursos("MySQL", "20/07/22"));
-cursos.push(new Cursos("Phyton", "21/07/22"));
-cursos.push(new Cursos("Angular", "22/07/22"));
 
-(function agregarAlumnos() {
-  let nombre;
-  do {
-    nombre = prompt("Ingresar el nombre del alumno");
-    if (nombre == "") {
-      break;
+function crearEstudiante() {
+  const nombre = prompt("Ingrese nombre del estudiante:");
+  const apellido = prompt("Ingrese apellido del estudiante:");
+  const id = prompt("Ingrese DNI del estudiante:");
+
+  return new Estudiante(nombre, apellido, id);
+}
+
+function crearCurso() {
+  const nombre = prompt("Ingrese nombre del curso:");
+  const fechaInicio = prompt("Ingrese fecha de inicio:");
+  const vacantes = prompt("Ingrese cantidad de vacantes:");
+
+  return new Curso(nombre, fechaInicio, vacantes);
+}
+
+/** 
+  * Programa que emula un sistema para registrar alumnos y cursos.
+
+  Acciones:
+    1. Crear alumno (nombre, apellido)
+    2. Crear cursos
+    3. Listar alumnos
+    4. Listar cursos
+    5. Eliminar alumno
+    6. Eliminar curso
+    7. Agregar Alumno a un curso
+    8. Mostrar detalles de un curso
+    9. Mostrar itinerario
+    12. Mostrar ficha alumno
+    0. Salir
+**/
+
+function main() {
+  let estudiantes = [];
+  let cursos = [
+    new Curso("Sistemas y Metodos", "17/07/22"),
+    new Curso("Introduccion a la programacion", "20/07/22"),
+  ];
+  let accion;
+  let operacion;
+
+  while (operacion != 0) {
+    accion = parseInt(prompt(MENU_info));
+
+    let estudianteId;
+    let curso;
+    let estudiante;
+
+    switch (accion) {
+      case ACCIONES.CrearEstudiante:
+        estudiante = crearEstudiante();
+        estudiantes.push(estudiante);
+        break;
+
+      case ACCIONES.CrearCurso:
+        curso = crearCurso();
+
+        if (cursos.some((c) => c.nombre === curso.nombre)) {
+          alert("Ya existe un curso con ese nombre");
+          break;
+        }
+
+        cursos.push(curso);
+        break;
+
+      case ACCIONES.ListadoEstudiantes:
+        const nombresFormateados = estudiantes
+          .map((s) => s.nombreCompleto())
+          .join("\n");
+        alert(`Lista de alumnos: \n ${nombresFormateados}`);
+        break;
+
+      case ACCIONES.ListadoCursos:
+        const cursosFormateados = cursos
+          .map(
+            (c) =>
+              `📎 ${c.nombre} - ${c.fechaInicio} (${c.estudiantes.length}/${c.vacantes} estudiantes)`
+          )
+          .join("\n");
+        alert(`Lista de cursos:\n ${cursosFormateados}`);
+        break;
+
+      case ACCIONES.BorrarEstudiante:
+        estudianteId = prompt("DNI del alumno a eliminar:");
+        estudiantes = estudiantes.filter((s) => s.id !== estudianteId);
+        break;
+
+      case ACCIONES.BorrarCurso:
+        const nombre = prompt("Nombre del curso a eliminar:");
+        cursos = cursos.filter((s) => s.nombre !== nombre);
+        break;
+
+      case ACCIONES.AgregarEstudianteACurso:
+        estudianteId = prompt("DNI del alumno:");
+        nombreCurso = prompt("Nombre del curso:");
+
+        estudiante = estudiantes.find((s) => s.id === estudianteId);
+        curso = cursos.find((c) => c.nombre === nombreCurso);
+
+        curso.agregarEstudiante(estudiante);
+        break;
+
+      case ACCIONES.MostrarCurso:
+        nombreCurso = prompt("Nombre del curso:");
+        curso = cursos.find((c) => c.nombre === nombreCurso);
+        alert(curso.info());
+        break;
+
+      case ACCIONES.MostrarAgenda:
+        const resultado = cursos.map((c) => c.info());
+        alert(`Itinerario: \n ${resultado.join("\n")}`);
+        break;
+
+      case ACCIONES.Salir:
+        operacion = 0;
+        break;
     }
+  }
+}
 
-    let apellido = prompt("Ingresar apellido");
-    let curso = prompt("Ingrese curso");
-    let alumno = new Alumnos(nombre, apellido, curso);
-    let buscarCurso = cursos.find((c) => c.nombre == curso);
-
-    if (buscarCurso) {
-      if (buscarCurso.cantidadDeAlumnos < 5) {
-        alumnos.push(alumno);
-        let index = cursos.findIndex((c) => c.nombre == curso);
-        cursos[index].cantidadDeAlumnos = cursos[index].cantidadDeAlumnos + 1;
-        alert(
-          "El cursos inicia " +
-            cursos[index].fechaIni +
-            " y la cantidad de incripciones es " +
-            cursos[index].cantidadDeAlumnos
-        );
-      } else {
-        alert("Cupo lleno");
-      }
-    } else {
-      alert("No existe el curso");
-    }
-  } while (nombre !== "");
-})();
+main();
